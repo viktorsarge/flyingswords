1ccd11cc3aeca605239c25d533eef8e728d33b6f
Viktor Sarge <viktorsarge@hotmail.com>
2018-09-11 13:49:09 +0200

    Cleanup

--- js/defaults.js
+++ js/defaults.js
@@ -26,7 +26,7 @@ var defaults = (function () {
     var levels = [
         {
             killsRequired: 4,
-            clockSpeed: 2200,
+            clockSpeed: 1200,
             numberOfObstacles: 15,
             simultEnemies: 1
         },
